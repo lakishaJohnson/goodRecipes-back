24407a8aa1145a2174247aff304649dbd57dfe0f
Lakisha J <lakishajohnson@pursuit.org>
2023-08-21 11:16:49 -0400

    Removed quick_recipe route

--- app.js
+++ app.js
@@ -8,9 +8,16 @@ app.use(express.json());
 const recipesController = require("./controllers/recipesController");
 app.use("/recipes", recipesController);
 
+const { getQuickRecipes } = require("./queries/recipes");
+
 // ROUTES
-app.get("/", (req, res) => {
-  res.send("Recipes R Us");
+app.get("/", async (req, res) => {
+  try {
+    const quickRecipesData = await getQuickRecipes();
+    res.json(quickRecipesData);
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
 });
 
 app.get("*", (req, res) => {
